d6923c9776b89eaab1bc7a6ddbe547f07f188729
officialanivox-afk <official.anivox@gmail.com>
2025-12-23 23:08:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("ANIVOX Loaded Successfully");
+function openReel() {
+  document.getElementById("reelScreen").style.display = "block";
+}
+
+function closeReel() {
+  document.getElementById("reelScreen").style.display = "none";
+}
