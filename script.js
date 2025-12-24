c83f591fb23a4dbae17539df9b42d2eff8486d53
officialanivox-afk <official.anivox@gmail.com>
2025-12-24 21:20:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,6 +53,7 @@ function applyRealm(i) {
   document.documentElement.style.setProperty("--bg1", r.bg1);
   document.documentElement.style.setProperty("--bg2", r.bg2);
   document.documentElement.style.setProperty("--accent", r.accent);
+  playRealmSound(index);
 }
 
 /* INITIAL */
