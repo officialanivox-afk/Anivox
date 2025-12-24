4fda8ad75d280019224ff0ef83281f6b9888c824
officialanivox-afk <official.anivox@gmail.com>
2025-12-24 20:47:56 +0530

    Update role.js

--- role.js
+++ role.js
@@ -2,13 +2,4 @@ function setRole(role) {
   localStorage.setItem("anivox_role", role);
   window.location.href = "realm.html";
 }
-setTimeout(() => {
-  const msg = document.createElement("div");
-  msg.innerText = "Welcome to ANIVOX 🌌";
-  msg.style.position = "fixed";
-  msg.style.bottom = "20px";
-  msg.style.left = "50%";
-  msg.style.transform = "translateX(-50%)";
-  msg.style.opacity = "0.7";
-  document.body.appendChild(msg);
-}, 2000);
+
