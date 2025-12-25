4a6c0de769726aa7cdf637468fb6d88bc43f9da0
officialanivox-afk <official.anivox@gmail.com>
2025-12-25 19:29:11 +0530

    Update role.js

--- role.js
+++ role.js
@@ -1,15 +1,1 @@
-function setRole() {
-  const roleScreen = document.getElementById("role-screen");
-  const world = document.getElementById("world");
 
-  // hide role screen
-  roleScreen.style.display = "none";
-
-  // show world
-  world.style.display = "block";
-  world.style.opacity = "1";
-
-  // trigger sound
-  const soundBtn = document.getElementById("soundBtn");
-  if (soundBtn) soundBtn.click();
-}
