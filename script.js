6015ee27683594cd9dd333252189555b23f12628
officialanivox-afk <official.anivox@gmail.com>
2025-12-26 18:04:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const portal = document.getElementById("portal");
 btn.addEventListener("click", () => {
   portal.classList.add("active");
 
-  document.body.style.transition = "transform 0.6s ease";
-  document.body.style.transform = "scale(1.1)";
+  document.body.style.transition = "transform 0.8s ease";
+  document.body.style.transform = "scale(1.15)";
 
   setTimeout(() => {
     window.location.href = "world.html";
-  }, 900);
+  }, 1000);
 });
