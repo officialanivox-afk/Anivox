8369a2093a09c64eaf70fd59a24251cda27d2987
officialanivox-afk <official.anivox@gmail.com>
2025-12-26 22:48:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-const enterBtn = document.getElementById("enterBtn");
-const portal = document.getElementById("portal-screen");
-const world = document.getElementById("world");
+function enterWorld() {
+  document.body.style.transition = "1s";
+  document.body.style.background = "#000";
 
-enterBtn.addEventListener("click", () => {
-  portal.style.opacity = "0";
-  portal.style.transform = "scale(1.2)";
   setTimeout(() => {
-    portal.style.display = "none";
-    world.classList.remove("hidden");
-  }, 900);
-});
+    window.location.href = "home.html";
+  }, 800);
+}
