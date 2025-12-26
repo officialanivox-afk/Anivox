5e0ce13ae5bc0f8553cb546aaa8f9753c278994d
officialanivox-afk <official.anivox@gmail.com>
2025-12-26 18:43:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const portal = document.getElementById("portal");
-const button = document.querySelector(".enter-btn");
-const screen = document.querySelector(".screen");
+const portal = document.querySelector('.portal-wrapper');
+const button = document.querySelector('.enter-btn');
 
 function enterWorld() {
-  screen.classList.add("fade-out");
+  portal.style.transform = 'scale(1.3)';
+  portal.style.boxShadow = '0 0 150px #00ffe0';
 
   setTimeout(() => {
     window.location.href = "world.html";
   }, 700);
 }
 
-portal.addEventListener("click", enterWorld);
-button.addEventListener("click", enterWorld);
+portal.addEventListener('click', enterWorld);
+button.addEventListener('click', enterWorld);
