d69982435fbed619885fa0cf6f23cf192c0e5a5b
officialanivox-afk <official.anivox@gmail.com>
2025-12-29 21:27:48 +0530

    Update anime.js

--- anime.js
+++ anime.js
@@ -2,11 +2,14 @@ const cards = document.querySelectorAll('.anime-card');
 
 cards.forEach(card => {
   card.addEventListener('click', () => {
-    card.classList.add('pulse');
+
+    card.style.transform = "scale(0.95)";
+    card.style.boxShadow = "0 0 60px rgba(155,92,255,0.9)";
 
     setTimeout(() => {
-      card.classList.remove('pulse');
-      alert("⚔️ Anime page coming soon...");
+      window.location.href = "anime-view.html";
+      window.location.href = "home.html";
     }, 300);
+
   });
 });
